6e560106d3424a95786066be4aadd45ed243659d
Richard Hopkins <hopkira@googlemail.com>
2020-12-19 20:54:18 +0000

    Ooops

--- decawave_ble/config/locate.py
+++ decawave_ble/config/locate.py
@@ -5,16 +5,6 @@ TAG_NAME = 'DW25CF'
 
 decawave_devices = decawave_ble.scan_for_decawave_devices()
 
-while True:
-    try:
-        scan_data = decawave_ble.get_data(decawave_devices[TAG_NAME])
-        position = scan_data['location_data']['position_data']
-        time.sleep(0.1)
-    except:
-        position = dict(x_postion = 0, y_position = 0)
-    vector = get_vector(position['x_position'], position['y_position'])
-    print('Dist: %2fm Angle: %2f radians ', (vector['dist'], vector['angle']))
-
 def get_vector(x,y):
     dist = get_dist(x,y)
     angle = get_angle(x,y)
@@ -25,3 +15,13 @@ def get_dist(x,y):
 
 def get_angle(x,y):
     return math.atan2(y,x)
+
+while True:
+    try:
+        scan_data = decawave_ble.get_data(decawave_devices[TAG_NAME])
+        position = scan_data['location_data']['position_data']
+        time.sleep(0.1)
+    except:
+        position = dict(x_postion = 0, y_position = 0)
+    vector = get_vector(position['x_position'], position['y_position'])
+    print('Dist: %2fm Angle: %2f radians ', (vector['dist'], vector['angle']))
